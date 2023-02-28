387098fe7992d5f23cac646fa60e0b6f0911befa
DiegoOCM <Diego1570565@gmail.com>
2023-02-28 10:19:54 -0300

    adicionando estilo

--- config.php
+++ config.php
@@ -1,36 +1,47 @@
 <?php
- use PHPMailer\PHPMailer\Exception;
- use PHPMailer\PHPMailer\PHPMailer;
+session_start();
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
 
- function Enviar_email($para, $assunto, $mensagem)
- {
+
+
+$para = $_SESSION['para'];
+$assunto = $_SESSION['assunto'];
+$mensagem = $_SESSION['mensagem'];
+
+
+Enviar_email($para , $assunto , $mensagem);
+
+function Enviar_email($para, $assunto, $mensagem)
+{
     header("Content-type: text/html; charset=utf-8");
 
-     require "PHPMailer/OAuth.php";
-     require "PHPMailer/PHPMailer.php";
-     require "PHPMailer/POP3.php";
-     require "PHPMailer/SMTP.php";
+    require "PHPMailer/OAuth.php";
+    require "PHPMailer/PHPMailer.php";
+    require "PHPMailer/POP3.php";
+    require "PHPMailer/SMTP.php";
 
     $mail = new PHPMailer(true);
     try {
         $mail->SMTPDebug = 2;
         $mail->isSMTP();
-        $mail->Host       = 'smtp.gmail.com';
-        $mail->SMTPAuth   = true;
-        $mail->Username = 'LojaUnnic@gmail.com';
-        $mail->Password = 'oammshsdagslfmmp';
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = $_SESSION['principal'];
+        $mail->Password = $_SESSION['senha'];
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port       = 587;
+        $mail->Port = 587;
 
         $mail->setFrom($para, 'UNNIC');
         $mail->addAddress($para, 'UNNIC');
         $mail->isHTML(true);
         $mail->Subject = $assunto;
-        $mail->Body    = $mensagem;
+        $mail->Body = $mensagem;
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
         $mail->send();
         echo 'Message has been sent';
+        header('location:index.php?email=enviado');
     } catch (Exception $e) {
-        echo "Tú digitou errado man, faz favor de fazer o trem direito: {$mail->ErrorInfo}";
+        header('location:index.php?email=nenviado');
     }
- }
+}
--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <title>App - Send Mail</title>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
         integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
         crossorigin="anonymous"></script>
@@ -25,7 +26,23 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
+
 <body>
-    
+    <div class="container">
+        <form action="tratamento.php" method="post">
+            <div class="m-5">
+                <span>Para quem é o Email:</span>
+                <input type="text" class="w-100" name="para" id="para">
+                <br>
+                <span>Qual o assunto:</span>
+                <input type="text" class="w-100" name="assunto" id="assunto">
+                <br>
+                <span>Conteudo do email:</span>
+                <textarea name="mensagem" class="w-100" id="mensagem" cols="30" rows="10"></textarea>
+                <input type="submit" class="btn btn-success " value="Enviar">
+            </div>
+        </form>
+    </div>
 </body>
+
 </html>
